1a077783da1a51a2a9dc619596b34ad1057feb45
hyo-lin <lin1137001@gmail.com>
2025-06-01 02:32:12 +0900

    feat: email과 socialtype으로 유저정보찾기 추가

---
+++ src/main/java/com/hanium/mom4u/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.hanium.mom4u.domain.member.repository;
+
+import com.hanium.mom4u.domain.member.common.SocialType;
+import com.hanium.mom4u.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmailAndSocialType(String email, SocialType socialType);
+}
