0ee011946cf15a260da15427ffaae984a14eb282
hyo-lin <lin1137001@gmail.com>
2025-06-01 17:11:03 +0900

    feat: email로 조회 구현

--- src/main/java/com/hanium/mom4u/domain/member/repository/MemberRepository.java
+++ src/main/java/com/hanium/mom4u/domain/member/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndSocialType(String email, SocialType socialType);
+    Optional<Member> findByEmail(String email);
+
 }
